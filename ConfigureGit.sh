f05cdf27d59884a2f23457100c60631cd1e5e580
Jake Hargrove <jvhargro@calpoly.edu>
2014-04-03 15:02:10 -0700

    track Congig script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+git config --global user.name "Jake Hargrove"
+git config --global user.email "jvhargro@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
